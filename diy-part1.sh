9cf3c6ae5831a0ea2939b680de95158de57d26a6
Hongjie <37176899+Carever@users.noreply.github.com>
2022-07-28 21:06:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,3 +1,6 @@
 git clone https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
 svn checkout https://github.com/openwrt/packages/trunk/libs/libnetfilter-queue package/libs/libnetfilter-queue
 git clone https://github.com/Zxilly/UA2F package/UA2F
+
+# K2-16M修改编译文件
+sed -i 's/7872k/15744k/g' target/linux/ramips/image/mt7620.mk
